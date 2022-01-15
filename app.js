a9c79f6365010ec5d54a5f0e2270285c83ef2927
Raghav Bang <raghavbang2012@gmail.com>
2022-01-15 14:55:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const btnTranslate = document.querySelector("#btn-tran")
 const data = document.querySelector("#inputdata")
 const out = document.querySelector("#output")
 const img_out = document.querySelector("#img")
-var url = "https://api.funtranslations.com/translate/ferb-latin.json?text="
+let url = "https://api.funtranslations.com/translate/ferb-latin.json?text="
 
-function errorHandler(error) {
-    console.log(error)
-}
+const errorHandler=(error) =>console.log(error)
 
 //milliseconds until timeout//
-function hideElement() {
+const hideElement=()=> {
     document.getElementById("img_o").src = ""
 }
 
-function messgae() {
+const messgae=()=> {
     url = url + data.value
     var data_out = fetch(url).then(response => response.json()).
     then(json => { out.innerText = json.contents.translated }).catch(errorHandler)
